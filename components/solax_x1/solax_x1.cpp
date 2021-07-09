2bf1019a19896ae4242c60e50d2a5d3aa1c91185
Sebastian Muszynski <basti@linkt.de>
2021-07-09 14:21:30 +0200

    Add cpp and python linter to the CI (#1)

--- components/solax_x1/solax_x1.cpp
+++ components/solax_x1/solax_x1.cpp
@@ -66,20 +66,6 @@ void SolaxX1::on_modbus_solax_info(const std::vector<uint8_t> &data) {
     return;
   }
 
-  // Request info (0x11 0x03)
-  // [18:13:12][VV][modbus_solax:214]: TX -> AA.55.01.00.00.0A.11.03.00.01.1E (11)
-  // Response (0x11 0x83)
-  // [18:13:12][VV][modbus_solax:084]: RX <- AA.55.00.0A.01.00.11.83.3A.01.00.00.00.00.00.00.56.31.2E.30.30.20.20.20.20.20.20.20.20.20.20.20.20.20.20.73.6F.6C.61.78.20.20.20.20.20.20.20.20.20.58.4D.55.30.36.32.47.43.30.39.33.35.34.30.33.36.30.30.0C.0F (69)
-  //                                                                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  //
-  // Data  0      (device type?):      0x01 (single phase)
-  // Data  1...6  (rated power):       0x00 0x00 0x00 0x00 0x00 0x00 ()
-  // Data  7...11 (firmware version):  0x56 0x31 0x2E 0x30 0x30 (V1.00)
-  // Data 12...25 (module name):       0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 ( )
-  // Data 26...39 (factory name):      0x73 0x6F 0x6C 0x61 0x78 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 0x20 (solax )
-  // Data 40...53 (serial number):     0x58 0x4D 0x55 0x30 0x36 0x32 0x47 0x43 0x30 0x39 0x33 0x35 0x34 0x30
-  // (XMU062GC093540) Data 54...57 (rated bus voltage): 0x33 0x36 0x30 0x30 (3600)
-
   ESP_LOGW(TAG, "Info: %s", hexencode(&data.front(), data.size()).c_str());
   // @TODO: Output Solax_Info_t
   this->no_response_count_ = 0;
@@ -90,37 +76,6 @@ void SolaxX1::on_modbus_solax_data(const std::vector<uint8_t> &data) {
     ESP_LOGW(TAG, "Invalid response size: %zu", data.size());
     return;
   }
-
-  // Request live data (0x11 0x02)
-  // [18:15:15][VV][modbus_solax:214]: TX -> AA.55.01.00.00.0A.11.02.00.01.1D (11)
-  // Response (0x11 0x82)
-  // [18:15:15][VV][modbus_solax:214]: RX <-
-  // AA.55.00.0A.01.00.11.82.34.00.19.00.01.02.46.00.00.00.0A.00.00.00.05.09.13.13.87.00.32.FF.FF.00.00.00.11.00.00.00.14.00.02.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.00.02.D0.06.21 (63)
-  //                            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  //
-  // Data  0...1  (temperature):        0x00 0x19 (25 C)
-  // Data  2...3  (energy today):       0x00 0x01 (0.1 kWh)
-  // Data  4...5  (pv1 voltage):        0x02 0x46 (58.2 V)
-  // Data  6...7  (pv2 voltage):        0x00 0x00 (0.0 V)
-  // Data  8...9  (pv1 current):        0x00 0x0A (1.0 A)
-  // Data 10...11 (pv2 current):        0x00 0x00 (0.0 A)
-  // Data 12...13 (ac current):         0x00 0x05 (0.5 A)
-  // Data 14...15 (ac voltage):         0x09 0x13 (232.3V)
-  // Data 16...17 (ac frequency):       0x13 0x87 (49.99 Hz)
-  // Data 18...19 (ac power):           0x00 0x32 (50 W)
-  // Data 20...21 (unused):             0xFF 0xFF
-  // Data 22...25 (energy total):       0x00 0x00 0x00 0x11 (0.1 kWh)
-  // Data 26...29 (runtime total):      0x00 0x00 0x00 0x14 (20 h)
-  // Data 30...31 (mode):               0x00 0x02 (2: Normal)
-  // Data 32...33 (grid voltage fault): 0x00 0x00 (0.0 V)
-  // Data 34...35 (grid freq. fault)    0x00 0x00 (0.00 Hz)
-  // Data 36...37 (dc injection fault): 0x00 0x00 (0 mA)
-  // Data 38...39 (temperature fault):  0x00 0x00
-  // Data 40...41 (pv1 voltage fault):  0x00 0x00
-  // Data 42...43 (pv2 voltage fault):  0x00 0x00
-  // Data 44...45 (gfc fault):          0x00 0x00
-  // Data 46...49 (error message):      0x00 0x00 0x00 0x00
-  // Data 50...52 (unknown):            0x02 0xD0
 
   auto solax_get_16bit = [&](size_t i) -> uint16_t {
     return (uint16_t(data[i + 0]) << 8) | (uint16_t(data[i + 1]) << 0);
