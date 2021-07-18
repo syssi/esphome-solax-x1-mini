be09db75a49197955c939c651006416af199e19f
Sebastian Muszynski <basti@linkt.de>
2021-07-18 11:53:19 +0200

    Rewrite sensor platform to a component acting as hub (#5)

--- components/solax_x1/sensor.py
+++ components/solax_x1/sensor.py
@@ -1,8 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor, modbus_solax
+from esphome.components import sensor
 from esphome.const import (
-    CONF_ID,
     CONF_MODE,
     CONF_TEMPERATURE,
     DEVICE_CLASS_EMPTY,
@@ -22,9 +21,13 @@ from esphome.const import (
     UNIT_WATT,
     UNIT_EMPTY,
 )
+from . import (
+    SolaxX1,
+    CONF_SOLAX_X1_ID,
+)
 
-AUTO_LOAD = ["modbus_solax"]
-CODEOWNERS = ["@syssi"]
+
+DEPENDENCIES = ["solax_x1"]
 
 CONF_ENERGY_TODAY = "energy_today"
 CONF_ENERGY_TOTAL = "energy_total"
@@ -62,89 +65,59 @@ SENSORS = [
     CONF_TEMPERATURE,
 ]
 
-solax_x1_ns = cg.esphome_ns.namespace("solax_x1")
-SolaxX1 = solax_x1_ns.class_(
-    "SolaxX1", cg.PollingComponent, modbus_solax.ModbusSolaxDevice
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(CONF_SOLAX_X1_ID): cv.use_id(SolaxX1),
+        cv.Optional(CONF_ENERGY_TODAY): sensor.sensor_schema(
+            UNIT_KILO_WATT_HOURS, ICON_COUNTER, 3, DEVICE_CLASS_ENERGY
+        ),
+        cv.Optional(CONF_ENERGY_TOTAL): sensor.sensor_schema(
+            UNIT_KILO_WATT_HOURS, ICON_COUNTER, 3, DEVICE_CLASS_ENERGY
+        ),
+        cv.Optional(CONF_DC1_CURRENT): sensor.sensor_schema(
+            UNIT_AMPERE, ICON_EMPTY, 1, DEVICE_CLASS_CURRENT
+        ),
+        cv.Optional(CONF_DC1_VOLTAGE): sensor.sensor_schema(
+            UNIT_VOLT, ICON_EMPTY, 1, DEVICE_CLASS_VOLTAGE
+        ),
+        cv.Optional(CONF_DC2_CURRENT): sensor.sensor_schema(
+            UNIT_AMPERE, ICON_EMPTY, 1, DEVICE_CLASS_CURRENT
+        ),
+        cv.Optional(CONF_DC2_VOLTAGE): sensor.sensor_schema(
+            UNIT_VOLT, ICON_EMPTY, 1, DEVICE_CLASS_VOLTAGE
+        ),
+        cv.Optional(CONF_AC_CURRENT): sensor.sensor_schema(
+            UNIT_AMPERE, ICON_EMPTY, 2, DEVICE_CLASS_CURRENT
+        ),
+        cv.Optional(CONF_AC_VOLTAGE): sensor.sensor_schema(
+            UNIT_VOLT, ICON_EMPTY, 1, DEVICE_CLASS_VOLTAGE
+        ),
+        cv.Optional(CONF_AC_FREQUENCY): sensor.sensor_schema(
+            UNIT_HERTZ, ICON_CURRENT_AC, 1, DEVICE_CLASS_EMPTY
+        ),
+        cv.Optional(CONF_AC_POWER): sensor.sensor_schema(
+            UNIT_WATT, ICON_EMPTY, 0, DEVICE_CLASS_POWER
+        ),
+        cv.Optional(CONF_RUNTIME_TOTAL): sensor.sensor_schema(
+            UNIT_HOURS, ICON_TIMER, 0, DEVICE_CLASS_EMPTY
+        ),
+        cv.Optional(CONF_ERROR_BITS): sensor.sensor_schema(
+            UNIT_EMPTY, ICON_ERROR_BITS, 0, DEVICE_CLASS_EMPTY
+        ),
+        cv.Optional(CONF_MODE): sensor.sensor_schema(
+            UNIT_EMPTY, ICON_MODE, 0, DEVICE_CLASS_EMPTY
+        ),
+        cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
+            UNIT_CELSIUS, ICON_EMPTY, 1, DEVICE_CLASS_TEMPERATURE
+        ),
+    }
 )
-
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(SolaxX1),
-            cv.Optional(CONF_ENERGY_TODAY): sensor.sensor_schema(
-                UNIT_KILO_WATT_HOURS, ICON_COUNTER, 3, DEVICE_CLASS_ENERGY
-            ),
-            cv.Optional(CONF_ENERGY_TOTAL): sensor.sensor_schema(
-                UNIT_KILO_WATT_HOURS, ICON_COUNTER, 3, DEVICE_CLASS_ENERGY
-            ),
-            cv.Optional(CONF_DC1_CURRENT): sensor.sensor_schema(
-                UNIT_AMPERE, ICON_EMPTY, 1, DEVICE_CLASS_CURRENT
-            ),
-            cv.Optional(CONF_DC1_VOLTAGE): sensor.sensor_schema(
-                UNIT_VOLT, ICON_EMPTY, 1, DEVICE_CLASS_VOLTAGE
-            ),
-            cv.Optional(CONF_DC2_CURRENT): sensor.sensor_schema(
-                UNIT_AMPERE, ICON_EMPTY, 1, DEVICE_CLASS_CURRENT
-            ),
-            cv.Optional(CONF_DC2_VOLTAGE): sensor.sensor_schema(
-                UNIT_VOLT, ICON_EMPTY, 1, DEVICE_CLASS_VOLTAGE
-            ),
-            cv.Optional(CONF_AC_CURRENT): sensor.sensor_schema(
-                UNIT_AMPERE, ICON_EMPTY, 2, DEVICE_CLASS_CURRENT
-            ),
-            cv.Optional(CONF_AC_VOLTAGE): sensor.sensor_schema(
-                UNIT_VOLT, ICON_EMPTY, 1, DEVICE_CLASS_VOLTAGE
-            ),
-            cv.Optional(CONF_AC_FREQUENCY): sensor.sensor_schema(
-                UNIT_HERTZ, ICON_CURRENT_AC, 1, DEVICE_CLASS_EMPTY
-            ),
-            cv.Optional(CONF_AC_POWER): sensor.sensor_schema(
-                UNIT_WATT, ICON_EMPTY, 0, DEVICE_CLASS_POWER
-            ),
-            cv.Optional(CONF_RUNTIME_TOTAL): sensor.sensor_schema(
-                UNIT_HOURS, ICON_TIMER, 0, DEVICE_CLASS_EMPTY
-            ),
-            cv.Optional(CONF_ERROR_BITS): sensor.sensor_schema(
-                UNIT_EMPTY, ICON_ERROR_BITS, 0, DEVICE_CLASS_EMPTY
-            ),
-            cv.Optional(CONF_MODE): sensor.sensor_schema(
-                UNIT_EMPTY, ICON_MODE, 0, DEVICE_CLASS_EMPTY
-            ),
-            cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
-                UNIT_CELSIUS, ICON_EMPTY, 1, DEVICE_CLASS_TEMPERATURE
-            ),
-        }
-    )
-    .extend(cv.polling_component_schema("30s"))
-    .extend(modbus_solax.modbus_solax_device_schema())
-)
-
-
-def validate_serial_number(value):
-    value = cv.string_strict(value)
-    parts = [value[i : i + 2] for i in range(0, len(value), 2)]
-    if len(parts) != 14:
-        raise cv.Invalid("Serial number must consist of 14 hexadecimal numbers")
-    parts_int = []
-    if any(len(part) != 2 for part in parts):
-        raise cv.Invalid("Serial number must be format XX")
-    for part in parts:
-        try:
-            parts_int.append(int(part, 16))
-        except ValueError:
-            # pylint: disable=raise-missing-from
-            raise cv.Invalid("Serial number must be hex values from 00 to FF")
-
-    return "".join(f"{part:02X}" for part in parts_int)
 
 
 def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield modbus_solax.register_modbus_solax_device(var, config)
-
+    hub = yield cg.get_variable(config[CONF_SOLAX_X1_ID])
     for key in SENSORS:
         if key in config:
             conf = config[key]
             sens = yield sensor.new_sensor(conf)
-            cg.add(getattr(var, f"set_{key}_sensor")(sens))
+            cg.add(getattr(hub, f"set_{key}_sensor")(sens))
