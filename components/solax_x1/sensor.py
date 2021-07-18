863a43ef58c926e55bbf19b7f15b14fc4c786b13
Sebastian Muszynski <basti@linkt.de>
2021-07-18 10:44:24 +0200

    Refactor sensor setup

--- components/solax_x1/sensor.py
+++ components/solax_x1/sensor.py
@@ -28,19 +28,14 @@ CODEOWNERS = ["@syssi"]
 
 CONF_ENERGY_TODAY = "energy_today"
 CONF_ENERGY_TOTAL = "energy_total"
-
 CONF_DC1_CURRENT = "dc1_current"
 CONF_DC1_VOLTAGE = "dc1_voltage"
-
 CONF_DC2_CURRENT = "dc2_current"
 CONF_DC2_VOLTAGE = "dc2_voltage"
-
 CONF_AC_CURRENT = "ac_current"
 CONF_AC_VOLTAGE = "ac_voltage"
-
 CONF_AC_FREQUENCY = "ac_frequency"
 CONF_AC_POWER = "ac_power"
-
 CONF_RUNTIME_TOTAL = "runtime_total"
 CONF_ERROR_BITS = "error_bits"
 
@@ -49,6 +44,23 @@ UNIT_KILO_WATT_HOURS = "kWh"
 
 ICON_MODE = "mdi:heart-pulse"
 ICON_ERROR_BITS = "mdi:alert-circle-outline"
+
+SENSORS = [
+    CONF_ENERGY_TODAY,
+    CONF_ENERGY_TOTAL,
+    CONF_DC1_CURRENT,
+    CONF_DC1_VOLTAGE,
+    CONF_DC2_CURRENT,
+    CONF_DC2_VOLTAGE,
+    CONF_AC_CURRENT,
+    CONF_AC_VOLTAGE,
+    CONF_AC_FREQUENCY,
+    CONF_AC_POWER,
+    CONF_RUNTIME_TOTAL,
+    CONF_ERROR_BITS,
+    CONF_MODE,
+    CONF_TEMPERATURE,
+]
 
 solax_x1_ns = cg.esphome_ns.namespace("solax_x1")
 SolaxX1 = solax_x1_ns.class_(
@@ -89,17 +101,17 @@ CONFIG_SCHEMA = (
             cv.Optional(CONF_AC_POWER): sensor.sensor_schema(
                 UNIT_WATT, ICON_EMPTY, 0, DEVICE_CLASS_POWER
             ),
-            cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
-                UNIT_CELSIUS, ICON_EMPTY, 1, DEVICE_CLASS_TEMPERATURE
-            ),
             cv.Optional(CONF_RUNTIME_TOTAL): sensor.sensor_schema(
                 UNIT_HOURS, ICON_TIMER, 0, DEVICE_CLASS_EMPTY
+            ),
+            cv.Optional(CONF_ERROR_BITS): sensor.sensor_schema(
+                UNIT_EMPTY, ICON_ERROR_BITS, 0, DEVICE_CLASS_EMPTY
             ),
             cv.Optional(CONF_MODE): sensor.sensor_schema(
                 UNIT_EMPTY, ICON_MODE, 0, DEVICE_CLASS_EMPTY
             ),
-            cv.Optional(CONF_ERROR_BITS): sensor.sensor_schema(
-                UNIT_EMPTY, ICON_ERROR_BITS, 0, DEVICE_CLASS_EMPTY
+            cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
+                UNIT_CELSIUS, ICON_EMPTY, 1, DEVICE_CLASS_TEMPERATURE
             ),
         }
     )
@@ -131,66 +143,8 @@ def to_code(config):
     yield cg.register_component(var, config)
     yield modbus_solax.register_modbus_solax_device(var, config)
 
-    if CONF_TEMPERATURE in config:
-        conf = config[CONF_TEMPERATURE]
-        sens = yield sensor.new_sensor(conf)
-        cg.add(var.set_temperature_sensor(sens))
-
-    if CONF_ENERGY_TODAY in config:
-        conf = config[CONF_ENERGY_TODAY]
-        sens = yield sensor.new_sensor(conf)
-        cg.add(var.set_energy_today_sensor(sens))
-    if CONF_ENERGY_TOTAL in config:
-        conf = config[CONF_ENERGY_TOTAL]
-        sens = yield sensor.new_sensor(conf)
-        cg.add(var.set_energy_total_sensor(sens))
-
-    if CONF_DC1_VOLTAGE in config:
-        conf = config[CONF_DC1_VOLTAGE]
-        sens = yield sensor.new_sensor(conf)
-        cg.add(var.set_dc1_voltage_sensor(sens))
-    if CONF_DC2_VOLTAGE in config:
-        conf = config[CONF_DC2_VOLTAGE]
-        sens = yield sensor.new_sensor(conf)
-        cg.add(var.set_dc2_voltage_sensor(sens))
-    if CONF_AC_VOLTAGE in config:
-        conf = config[CONF_AC_VOLTAGE]
-        sens = yield sensor.new_sensor(conf)
-        cg.add(var.set_ac_voltage_sensor(sens))
-
-    if CONF_DC1_CURRENT in config:
-        conf = config[CONF_DC1_CURRENT]
-        sens = yield sensor.new_sensor(conf)
-        cg.add(var.set_dc1_current_sensor(sens))
-    if CONF_DC2_CURRENT in config:
-        conf = config[CONF_DC2_CURRENT]
-        sens = yield sensor.new_sensor(conf)
-        cg.add(var.set_dc2_current_sensor(sens))
-    if CONF_AC_CURRENT in config:
-        conf = config[CONF_AC_CURRENT]
-        sens = yield sensor.new_sensor(conf)
-        cg.add(var.set_ac_current_sensor(sens))
-
-    if CONF_AC_FREQUENCY in config:
-        conf = config[CONF_AC_FREQUENCY]
-        sens = yield sensor.new_sensor(conf)
-        cg.add(var.set_ac_frequency_sensor(sens))
-
-    if CONF_AC_POWER in config:
-        conf = config[CONF_AC_POWER]
-        sens = yield sensor.new_sensor(conf)
-        cg.add(var.set_ac_power_sensor(sens))
-
-    if CONF_RUNTIME_TOTAL in config:
-        conf = config[CONF_RUNTIME_TOTAL]
-        sens = yield sensor.new_sensor(conf)
-        cg.add(var.set_runtime_total_sensor(sens))
-
-    if CONF_MODE in config:
-        conf = config[CONF_MODE]
-        sens = yield sensor.new_sensor(conf)
-        cg.add(var.set_mode_sensor(sens))
-    if CONF_ERROR_BITS in config:
-        conf = config[CONF_ERROR_BITS]
-        sens = yield sensor.new_sensor(conf)
-        cg.add(var.set_error_bits_sensor(sens))
+    for key in SENSORS:
+        if key in config:
+            conf = config[key]
+            sens = yield sensor.new_sensor(conf)
+            cg.add(getattr(var, f"set_{key}_sensor")(sens))
