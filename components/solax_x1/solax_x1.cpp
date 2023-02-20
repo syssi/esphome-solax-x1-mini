17ced995d2d6b191ccaf379bb9997a2858472d08
Sebastian Muszynski <basti@linkt.de>
2023-02-20 10:25:36 +0100

    Add sensors for measured faults (Closes: #25) (#42)

--- components/solax_x1/solax_x1.cpp
+++ components/solax_x1/solax_x1.cpp
@@ -223,41 +223,23 @@ void SolaxX1::decode_status_report_(const std::vector<uint8_t> &data) {
 
   ESP_LOGI(TAG, "Status frame received");
 
-  float temperature = (int16_t) solax_get_16bit(0);
-
-  uint16_t raw_energy_today = solax_get_16bit(2);
-  float energy_today = raw_energy_today * 0.1f;
-
-  uint16_t raw_dc1_voltage = solax_get_16bit(4);
-  float dc1_voltage = raw_dc1_voltage * 0.1f;
-
-  uint16_t raw_dc2_voltage = solax_get_16bit(6);
-  float dc2_voltage = raw_dc2_voltage * 0.1f;
-
-  uint16_t raw_dc1_current = solax_get_16bit(8);
-  float dc1_current = raw_dc1_current * 0.1f;
-
-  uint16_t raw_dc2_current = solax_get_16bit(10);
-  float dc2_current = raw_dc2_current * 0.1f;
-
-  uint16_t raw_ac_current = solax_get_16bit(12);
-  float ac_current = raw_ac_current * 0.1f;
-
-  uint16_t raw_ac_voltage = solax_get_16bit(14);
-  float ac_voltage = raw_ac_voltage * 0.1f;
-
-  uint16_t raw_ac_frequency = solax_get_16bit(16);
-  float ac_frequency = raw_ac_frequency * 0.01f;
-
-  uint16_t raw_ac_power = solax_get_16bit(18);
-  float ac_power = raw_ac_power;
+  this->publish_state_(this->temperature_sensor_, (int16_t) solax_get_16bit(0));
+  this->publish_state_(this->energy_today_sensor_, solax_get_16bit(2) * 0.1f);
+  this->publish_state_(this->dc1_voltage_sensor_, solax_get_16bit(4) * 0.1f);
+  this->publish_state_(this->dc2_voltage_sensor_, solax_get_16bit(6) * 0.1f);
+  this->publish_state_(this->dc1_current_sensor_, solax_get_16bit(8) * 0.1f);
+  this->publish_state_(this->dc2_current_sensor_, solax_get_16bit(10) * 0.1f);
+  this->publish_state_(this->ac_current_sensor_, solax_get_16bit(12) * 0.1f);
+  this->publish_state_(this->ac_voltage_sensor_, solax_get_16bit(14) * 0.1f);
+  this->publish_state_(this->ac_frequency_sensor_, solax_get_16bit(16) * 0.01f);
+  this->publish_state_(this->ac_power_sensor_, solax_get_16bit(18));
 
   // register 20 is not used
 
   uint32_t raw_energy_total = solax_get_32bit(22);
   // The inverter publishes a zero once per day on boot-up. This confuses the energy dashboard.
   if (raw_energy_total > 0) {
-    this->publish_state_(this->energy_total_sensor_, (float) raw_energy_total * 0.1f);
+    this->publish_state_(this->energy_total_sensor_, raw_energy_total * 0.1f);
   }
 
   uint32_t raw_runtime_total = solax_get_32bit(26);
@@ -266,35 +248,24 @@ void SolaxX1::decode_status_report_(const std::vector<uint8_t> &data) {
   }
 
   uint8_t mode = (uint8_t) solax_get_16bit(30);
+  this->publish_state_(this->mode_sensor_, mode);
+  this->publish_state_(this->mode_name_text_sensor_, (mode <= 9) ? MODE_NAMES[mode] : "Unknown");
 
-  ESP_LOGD(TAG, "  Grid voltage fault: %f V", solax_get_16bit(32) * 0.1f);
-  ESP_LOGD(TAG, "  Grid frequency fault: %f Hz", solax_get_16bit(34) * 0.01f);
-  ESP_LOGD(TAG, "  DC injection fault: %f A", solax_get_16bit(36) * 0.001f);
-  ESP_LOGD(TAG, "  Temperature fault: %d °C", solax_get_16bit(38));
-  ESP_LOGD(TAG, "  PV1 voltage fault: %f V", solax_get_16bit(40) * 0.1f);
-  ESP_LOGD(TAG, "  PV2 voltage fault: %f V", solax_get_16bit(42) * 0.1f);
-  ESP_LOGD(TAG, "  GFC fault: %f A", solax_get_16bit(44) * 0.001f);
+  this->publish_state_(this->grid_voltage_fault_sensor_, solax_get_16bit(32) * 0.1f);
+  this->publish_state_(this->grid_frequency_fault_sensor_, solax_get_16bit(34) * 0.01f);
+  this->publish_state_(this->dc_injection_fault_sensor_, solax_get_16bit(36) * 0.001f);
+  this->publish_state_(this->temperature_fault_sensor_, (float) solax_get_16bit(38));
+  this->publish_state_(this->pv1_voltage_fault_sensor_, solax_get_16bit(40) * 0.1f);
+  this->publish_state_(this->pv2_voltage_fault_sensor_, solax_get_16bit(42) * 0.1f);
+  this->publish_state_(this->gfc_fault_sensor_, solax_get_16bit(44) * 0.001f);
 
   uint32_t error_bits = solax_get_error_bitmask(46);
+  this->publish_state_(this->error_bits_sensor_, error_bits);
+  this->publish_state_(this->errors_text_sensor_, this->error_bits_to_string_(error_bits));
 
   if (data.size() > 50) {
     ESP_LOGD(TAG, "  CT Pgrid: %d W", solax_get_16bit(50));
   }
-
-  this->publish_state_(this->temperature_sensor_, temperature);
-  this->publish_state_(this->energy_today_sensor_, energy_today);
-  this->publish_state_(this->dc1_voltage_sensor_, dc1_voltage);
-  this->publish_state_(this->dc2_voltage_sensor_, dc2_voltage);
-  this->publish_state_(this->dc1_current_sensor_, dc1_current);
-  this->publish_state_(this->dc2_current_sensor_, dc2_current);
-  this->publish_state_(this->ac_current_sensor_, ac_current);
-  this->publish_state_(this->ac_voltage_sensor_, ac_voltage);
-  this->publish_state_(this->ac_frequency_sensor_, ac_frequency);
-  this->publish_state_(this->ac_power_sensor_, ac_power);
-  this->publish_state_(this->mode_sensor_, mode);
-  this->publish_state_(this->error_bits_sensor_, error_bits);
-  this->publish_state_(this->errors_text_sensor_, this->error_bits_to_string_(error_bits));
-  this->publish_state_(this->mode_name_text_sensor_, (mode <= 9) ? MODE_NAMES[mode] : "Unknown");
 
   this->no_response_count_ = 0;
 }
@@ -312,6 +283,13 @@ void SolaxX1::publish_device_offline_() {
   this->publish_state_(this->ac_voltage_sensor_, NAN);
   this->publish_state_(this->ac_frequency_sensor_, NAN);
   this->publish_state_(this->ac_power_sensor_, 0);
+  this->publish_state_(this->grid_voltage_fault_sensor_, NAN);
+  this->publish_state_(this->grid_frequency_fault_sensor_, NAN);
+  this->publish_state_(this->dc_injection_fault_sensor_, NAN);
+  this->publish_state_(this->temperature_fault_sensor_, NAN);
+  this->publish_state_(this->pv1_voltage_fault_sensor_, NAN);
+  this->publish_state_(this->pv2_voltage_fault_sensor_, NAN);
+  this->publish_state_(this->gfc_fault_sensor_, NAN);
 }
 
 void SolaxX1::update() {
@@ -347,20 +325,27 @@ void SolaxX1::dump_config() {
   ESP_LOGCONFIG(TAG, "SolaxX1:");
   ESP_LOGCONFIG(TAG, "  Address: 0x%02X", this->address_);
   LOG_SENSOR("", "Temperature", this->temperature_sensor_);
-  LOG_SENSOR("", "Energy Today", this->energy_today_sensor_);
-  LOG_SENSOR("", "DC1 Voltage", this->dc1_voltage_sensor_);
-  LOG_SENSOR("", "DC2 Voltage", this->dc2_voltage_sensor_);
-  LOG_SENSOR("", "DC1 Current", this->dc1_current_sensor_);
-  LOG_SENSOR("", "DC2 Current", this->dc2_current_sensor_);
-  LOG_SENSOR("", "AC Current", this->ac_current_sensor_);
-  LOG_SENSOR("", "AC Voltage", this->ac_voltage_sensor_);
-  LOG_SENSOR("", "AC Frequency", this->ac_frequency_sensor_);
-  LOG_SENSOR("", "AC Power", this->ac_power_sensor_);
-  LOG_SENSOR("", "Energy Total", this->energy_total_sensor_);
-  LOG_SENSOR("", "Runtime Total", this->runtime_total_sensor_);
+  LOG_SENSOR("", "Energy today", this->energy_today_sensor_);
+  LOG_SENSOR("", "DC1 voltage", this->dc1_voltage_sensor_);
+  LOG_SENSOR("", "DC2 voltage", this->dc2_voltage_sensor_);
+  LOG_SENSOR("", "DC1 current", this->dc1_current_sensor_);
+  LOG_SENSOR("", "DC2 current", this->dc2_current_sensor_);
+  LOG_SENSOR("", "AC current", this->ac_current_sensor_);
+  LOG_SENSOR("", "AC voltage", this->ac_voltage_sensor_);
+  LOG_SENSOR("", "AC frequency", this->ac_frequency_sensor_);
+  LOG_SENSOR("", "AC power", this->ac_power_sensor_);
+  LOG_SENSOR("", "Energy total", this->energy_total_sensor_);
+  LOG_SENSOR("", "Runtime total", this->runtime_total_sensor_);
   LOG_SENSOR("", "Mode", this->mode_sensor_);
-  LOG_SENSOR("", "Error Bits", this->error_bits_sensor_);
-  LOG_TEXT_SENSOR("  ", "Mode Name", this->mode_name_text_sensor_);
+  LOG_SENSOR("", "Error bits", this->error_bits_sensor_);
+  LOG_SENSOR("", "Grid voltage fault", this->grid_voltage_fault_sensor_);
+  LOG_SENSOR("", "Grid frequency fault", this->grid_frequency_fault_sensor_);
+  LOG_SENSOR("", "DC injection fault", this->dc_injection_fault_sensor_);
+  LOG_SENSOR("", "Temperature fault", this->temperature_fault_sensor_);
+  LOG_SENSOR("", "PV1 voltage fault", this->pv1_voltage_fault_sensor_);
+  LOG_SENSOR("", "PV2 voltage fault", this->pv2_voltage_fault_sensor_);
+  LOG_SENSOR("", "GFC fault", this->gfc_fault_sensor_);
+  LOG_TEXT_SENSOR("  ", "Mode name", this->mode_name_text_sensor_);
   LOG_TEXT_SENSOR("  ", "Errors", this->errors_text_sensor_);
 }
 
