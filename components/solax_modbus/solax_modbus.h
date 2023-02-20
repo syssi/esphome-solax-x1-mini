234eb81958a06b2635238d168cded3dce389d325
Sebastian Muszynski <basti@linkt.de>
2023-02-20 11:50:49 +0100

    BREAKING: Rename modbus_solax to solax_modbus (#45)

--- components/solax_modbus/solax_modbus.h
+++ components/solax_modbus/solax_modbus.h
@@ -4,7 +4,7 @@
 #include "esphome/components/uart/uart.h"
 
 namespace esphome {
-namespace modbus_solax {
+namespace solax_modbus {
 
 struct SolaxMessageT {
   uint8_t Header[2];
@@ -16,18 +16,18 @@ struct SolaxMessageT {
   uint8_t Data[100];
 };
 
-class ModbusSolaxDevice;
+class SolaxModbusDevice;
 
-class ModbusSolax : public uart::UARTDevice, public Component {
+class SolaxModbus : public uart::UARTDevice, public Component {
  public:
-  ModbusSolax() = default;
+  SolaxModbus() = default;
 
   void setup() override;
   void loop() override;
 
   void dump_config() override;
 
-  void register_device(ModbusSolaxDevice *device) { this->devices_.push_back(device); }
+  void register_device(SolaxModbusDevice *device) { this->devices_.push_back(device); }
   void set_flow_control_pin(GPIOPin *flow_control_pin) { this->flow_control_pin_ = flow_control_pin; }
 
   float get_setup_priority() const override;
@@ -40,20 +40,20 @@ class ModbusSolax : public uart::UARTDevice, public Component {
   void register_address(uint8_t serial_number[14], uint8_t address);
 
  protected:
-  bool parse_modbus_solax_byte_(uint8_t byte);
+  bool parse_solax_modbus_byte_(uint8_t byte);
   GPIOPin *flow_control_pin_{nullptr};
 
   std::vector<uint8_t> rx_buffer_;
-  uint32_t last_modbus_solax_byte_{0};
-  std::vector<ModbusSolaxDevice *> devices_;
+  uint32_t last_solax_modbus_byte_{0};
+  std::vector<SolaxModbusDevice *> devices_;
 };
 
-class ModbusSolaxDevice {
+class SolaxModbusDevice {
  public:
-  void set_parent(ModbusSolax *parent) { parent_ = parent; }
+  void set_parent(SolaxModbus *parent) { parent_ = parent; }
   void set_address(uint8_t address) { address_ = address; }
   void set_serial_number(uint8_t *serial_number) { serial_number_ = serial_number; }
-  virtual void on_modbus_solax_data(const uint8_t &function, const std::vector<uint8_t> &data) = 0;
+  virtual void on_solax_modbus_data(const uint8_t &function, const std::vector<uint8_t> &data) = 0;
 
   void query_status_report(uint8_t address) { this->parent_->query_status_report(address); }
   void query_device_info(uint8_t address) { this->parent_->query_device_info(address); }
@@ -61,12 +61,12 @@ class ModbusSolaxDevice {
   void discover_devices() { this->parent_->discover_devices(); }
 
  protected:
-  friend ModbusSolax;
+  friend SolaxModbus;
 
-  ModbusSolax *parent_;
+  SolaxModbus *parent_;
   uint8_t address_;
   uint8_t *serial_number_;
 };
 
-}  // namespace modbus_solax
+}  // namespace solax_modbus
 }  // namespace esphome
--- components/solax_x1/__init__.py
+++ components/solax_x1/__init__.py
@@ -1,9 +1,9 @@
 import esphome.codegen as cg
-from esphome.components import modbus_solax
+from esphome.components import solax_modbus
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-AUTO_LOAD = ["modbus_solax", "sensor", "text_sensor"]
+AUTO_LOAD = ["solax_modbus", "sensor", "text_sensor"]
 CODEOWNERS = ["@syssi"]
 MULTI_CONF = True
 
@@ -11,14 +11,14 @@ CONF_SOLAX_X1_ID = "solax_x1_id"
 
 solax_x1_ns = cg.esphome_ns.namespace("solax_x1")
 SolaxX1 = solax_x1_ns.class_(
-    "SolaxX1", cg.PollingComponent, modbus_solax.ModbusSolaxDevice
+    "SolaxX1", cg.PollingComponent, solax_modbus.SolaxModbusDevice
 )
 
 CONFIG_SCHEMA = (
     cv.Schema({cv.GenerateID(): cv.declare_id(SolaxX1)})
     .extend(cv.polling_component_schema("30s"))
     .extend(
-        modbus_solax.modbus_solax_device_schema(0x0A, "3132333435363737363534333231")
+        solax_modbus.solax_modbus_device_schema(0x0A, "3132333435363737363534333231")
     )
 )
 
@@ -26,4 +26,4 @@ CONFIG_SCHEMA = (
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await modbus_solax.register_modbus_solax_device(var, config)
+    await solax_modbus.register_solax_modbus_device(var, config)
