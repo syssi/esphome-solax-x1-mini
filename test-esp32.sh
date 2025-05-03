a348b3df81c923b2e1caf9bfb473446bdcfec5bd
Sebastian Muszynski <basti@linkt.de>
2025-05-03 20:44:01 +0200

    Adopt structure of the ESPHome CI (#75)

--- test-esp32.sh
+++ test-esp32.sh
@@ -6,4 +6,4 @@ else
   C="components"
 fi
 
-esphome -s external_components_source $C ${1:-run} ${2:-esp8266-example.yaml}
+esphome -s external_components_source $C ${1:-run} ${2:-esp32-example.yaml}
